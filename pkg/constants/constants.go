16d731e81002563f287b3ed626688e4322585c20
Ahmed Kadri <ahmed.kadri@spectrocloud.com>
2026-08-31 18:25:58 +0200

    PPD-1608: Upgrade vcluster to 0.36.1 (DRAFT)

--- pkg/constants/constants.go
+++ pkg/constants/constants.go
@@ -3,10 +3,8 @@ package constants
 import "os"
 
 var (
-	// DefaultVClusterVersion is the default version of the virtual cluster to use for NEW clusters.
-	// Existing clusters keep the version pinned on their VCluster CR (e.g. 0.27.3) and the matching
-	// chart is retained under charts/ so they keep reconciling unchanged.
-	DefaultVClusterVersion = "0.34.7"
+	// DefaultVClusterVersion is the default version of the virtual cluster to use
+	DefaultVClusterVersion = "0.36.1"
 
 	// DefaultVClusterChartName is the default chart name of the virtual cluster to use
 	DefaultVClusterChartName = "vcluster"
